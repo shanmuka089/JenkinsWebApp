03c42d4b93ce5bb621e8328cb1b160d73eb0cc96
shanmuka089 <shanmuka089@gmail.com>
2022-08-19 12:55:29 +0530

    Web-App updated & did some changes

---
+++ src/main/java/com/hello/MyController.java
@@ -0,0 +1,14 @@
+package com.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+	
+	@RequestMapping("/")
+	public String show() {
+		return "index";
+	}
+
+}
